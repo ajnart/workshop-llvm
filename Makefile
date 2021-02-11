ff6dcb84b3d07342aaa1b54131f48c9ca6b5fb13
ajnart <thomas.camlong@epitech.eu>
2021-02-11 22:07:32 +0100

    Added dirty code that needs cleanup

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ dirty.cpp
---
+++ dirty.cpp
@@ -0,0 +1,10 @@
+int divide(int x ,int y)
+{ return (((x/  y  )));
+}
+
+#include "iostream"
+
+int        main(int argc, char const *argv[]) {
+    divide(3, 0);
+    std::cout << "Hello, world!" << std::endl;
+    return 0; }
